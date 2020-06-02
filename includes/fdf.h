859e0067ffa1e07849bd48abcd367fe029c06904
ykalashn <ykalashn@student.hive.fi>
2020-06-02 10:43:09 +0300

    removed unnecessary files

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: ykalashn <ykalashn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/01 13:06:50 by ykalashn          #+#    #+#             */
-/*   Updated: 2020/06/02 10:04:31 by ykalashn         ###   ########.fr       */
+/*   Updated: 2020/06/02 10:41:51 by ykalashn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 # include <mlx.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include "../libft/libft.h"
+# include "../libft/includes/libft.h"
 
 void    ft_draw_line(int line[4], void **params, unsigned c);
 int     ft_abs(int nb);
